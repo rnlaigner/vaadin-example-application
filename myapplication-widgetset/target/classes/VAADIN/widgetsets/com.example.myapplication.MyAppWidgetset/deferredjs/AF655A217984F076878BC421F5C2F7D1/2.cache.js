52848f2b9237079b1e8f38541de4e4ad53401643
Rodrigo Laigner <rodrigolaigner@gmail.com>
2017-05-28 13:56:08 -0300

    first commit

---
+++ myapplication-widgetset/target/classes/VAADIN/widgetsets/com.example.myapplication.MyAppWidgetset/deferredjs/AF655A217984F076878BC421F5C2F7D1/2.cache.js
@@ -0,0 +1,1 @@
+$wnd.com_example_myapplication_MyAppWidgetset.runAsyncCallback2('pUb(1703,1,{203:1,1:1});_.mdb=function cRd(){this.ndb(this.b)};_.ndb=function dRd(a){this.odb(a)};_.odb=function eRd(a){};_.Uf=function gRd(){this.mdb();pid().k3(this.a.$c())};cmg(mq)(2);\n//# sourceURL=com.example.myapplication.MyAppWidgetset-2.js\n')
